3d3255734e77e48f605a3c4115666250be8fa4fa
MariaForester <maschalessnichaya@gmail.com>
2019-06-25 16:59:26 +0300

    Main screen + Internet connection check

--- app/src/main/java/com/lowlevelprog/lowlevelprogrammer/MainActivity.java
+++ app/src/main/java/com/lowlevelprog/lowlevelprogrammer/MainActivity.java
@@ -1,10 +1,16 @@
 package com.lowlevelprog.lowlevelprogrammer;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.DialogInterface;
+import android.view.View;
+import android.widget.Button;
+
 import com.hanks.htextview.base.AnimationListener;
 import com.hanks.htextview.base.HTextView;
 import com.hanks.htextview.line.LineTextView;
@@ -12,16 +18,41 @@ import com.hanks.htextview.line.LineTextView;
 public class MainActivity extends AppCompatActivity {
 
     private LineTextView hTextView;
+    private Button button_local, button_multiplayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        hTextView = (LineTextView) findViewById(R.id.tv_htext);
+        hTextView = findViewById(R.id.tv_htext); // block on above
         hTextView.setLineColor(R.color.textColor);
         hTextView.setAnimationListener(new SimpleAnimationListener(this));
         hTextView.animateText(getString(R.string.app_full_name));
+
+        button_local = findViewById(R.id.but_loc_game); // open one of game modes
+        button_multiplayer = findViewById(R.id.but_multip_game);
+        button_local.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGameMode(v.getId());
+            }
+        });
+        button_multiplayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGameMode(v.getId());
+            }
+        });
+    }
+
+    public void openGameMode(int identifier) {
+        Intent intent;
+        if (identifier == R.id.but_loc_game)
+            intent = new Intent(this, LocalGame.class);
+        else
+            intent = new Intent(this, MultiplayerGame.class);
+        startActivity(intent);
     }
 
     class SimpleAnimationListener implements AnimationListener {
@@ -31,10 +62,12 @@ public class MainActivity extends AppCompatActivity {
         SimpleAnimationListener(Context context) {
             this.context = context;
         }
+
         @Override
         public void onAnimationEnd(HTextView hTextView) {
             hTextView.animateText(getString(R.string.app_full_name));
         }
+
     }
 
     @Override
@@ -54,6 +87,53 @@ public class MainActivity extends AppCompatActivity {
         });
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!isConnected(MainActivity.this)) {
+            buildDialog(MainActivity.this).show();
+        }
+
+    }
+
+    public boolean isConnected(Context context) throws NullPointerException {
+
+        ConnectivityManager cm = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netinfo = cm.getActiveNetworkInfo();
+
+        if (netinfo != null && netinfo.isConnectedOrConnecting()) {
+            android.net.NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            android.net.NetworkInfo mobile = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+            return (mobile != null && mobile.isConnectedOrConnecting()) || (wifi != null &&
+                    wifi.isConnectedOrConnecting());
+        } else
+            return false;
+    }
+
+    public AlertDialog.Builder buildDialog(Context c) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        builder.setCancelable(false);
+        builder.setTitle("No Internet Connection");
+        builder.setMessage("Connect Your Device To Continue");
+        builder.setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("Continue", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent i = new Intent(MainActivity.this, Splash_Intro.class);
+                finish();
+                startActivity(i);
+            }
+        });
+        return builder;
     }
 }
 
---
+++ app/src/main/java/com/lowlevelprog/lowlevelprogrammer/MultiplayerGame.java
@@ -0,0 +1,13 @@
+package com.lowlevelprog.lowlevelprogrammer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultiplayerGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mulpiplayer_game);
+    }
+}
